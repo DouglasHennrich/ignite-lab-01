c8127a11da1e63d8d740ca249d6840d5a637c39a
Douglas Hennrich <douglas_b_h@hotmail.com>
2025-04-24 10:26:25 -0300

    criando a primeira mutation e apresentando os conceitos para criar uma mutation

--- purchases/src/products/products.service.ts
+++ purchases/src/products/products.service.ts
@@ -1,5 +1,11 @@
-import { PrismaService } from '@/database/prisma.service';
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+
+import { PrismaService } from '@/database/prisma.service';
+
+interface ICreateProductDTO {
+  title: string;
+}
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +13,24 @@ export class ProductsService {
 
   async listAllProducts() {
     return this.prisma.product.findMany();
+  }
+
+  async createProduct({ title }: ICreateProductDTO) {
+    const slug = slugify(title, { lower: true });
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: { slug },
+    });
+
+    if (productWithSameSlug) {
+      throw new Error('Product already exists');
+    }
+
+    return this.prisma.product.create({
+      data: {
+        title,
+        slug,
+        price: 0,
+      },
+    });
   }
 }
